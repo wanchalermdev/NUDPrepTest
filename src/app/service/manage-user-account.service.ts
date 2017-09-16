3baafbb732fc8d2fc972d23fb36510767c55ed29
wanchalermdev <wanchalerm.dev@gmail.com>
2017-09-16 14:30:50 +0700

    เพิ่ม createUserAccount(userAccountData) ใน Service สำหรับ REST ไปจัดการบัญชีผู้ใช้

--- src/app/service/manage-user-account.service.ts
+++ src/app/service/manage-user-account.service.ts
@@ -8,40 +8,54 @@ export class ManageUserAccountService {
   private allUserData;
 
   constructor(private _http: Http) {
-      this.allUserData = null;
+    this.allUserData = null;
   }
 
   getAllUserAccountFromBackEnd() {
 
-   this._http.get('http://10.41.131.180/NUDPrepTestBackEnd/user_account/UserAccountManagement.php').subscribe((data) => {
-    
-    
-    this.allUserData = data.json();
+    this._http.get('http://10.41.131.180/NUDPrepTestBackEnd/user_account/UserAccountManagement.php').subscribe((data) => {
 
-    if (this.allUserData['operation'] == "success"){
-      this.allUserData = this.allUserData['body'];
-      //console.log(JSON.stringify(this.allUserData));
-      //this.setAllUserData(this.allUserData['body']);
-      
-      window.sessionStorage.setItem('body', JSON.stringify(this.allUserData));
-    }
-    
-    // console.log(this.allUserData);
+
+      this.allUserData = data.json();
+
+      if (this.allUserData['operation'] == "success") {
+        this.allUserData = this.allUserData['body'];
+
+        window.sessionStorage.setItem('body', JSON.stringify(this.allUserData));
+      }
 
     });
-    // console.log(this.allUserData);
-    // return this.allUserData;
   }
   setAllUserData(data) {
     this.allUserData = data;
     // console.log(data);
   }
-  getAllUserAccount(){
+  getAllUserAccount() {
     setTimeout(() => {
       this.getAllUserAccountFromBackEnd();
     }, 2000);
-    
+
     return this.allUserData;
   }
-  
+
+  createUserAccount(userAccountData) {
+    /*
+    * ตั้งค่า Header application/x-www-form-urlencode'
+    */
+    var headers = new Headers();
+    headers.append('Content-Type', 'application/x-www-form-urlencoded');
+
+    /*
+    * pack parameter สำหรับส่งค่าไปสร้างบัญชีผู้ใช้
+    */
+    var _parameter = Object.keys(userAccountData).map(function (key) {
+      return encodeURIComponent(key) + '=' + encodeURIComponent(userAccountData[key]);
+    }).join('&');
+
+    return this._http.post('http://10.41.131.180/NUDPrepTestBackEnd/user_account/UserAccountManagement.php', _parameter, { headers: headers }).subscribe((data) => {
+      var responseJSON = data.json();
+      return responseJSON;
+    });
+
+  }
 }
