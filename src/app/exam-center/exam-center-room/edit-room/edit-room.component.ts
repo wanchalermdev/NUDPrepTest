5639671c78da629452e0ab4cef5ad5c0129a718d
wanchalermdev <wanchalerm.dev@gmail.com>
2017-09-18 15:15:01 +0700

    แก้ไขห้องสอบ

--- src/app/exam-center/exam-center-room/edit-room/edit-room.component.ts
+++ src/app/exam-center/exam-center-room/edit-room/edit-room.component.ts
@@ -1,13 +1,24 @@
 import { Component, OnInit } from '@angular/core';
-
+import { RoomManagementService } from '../../../service/room-management.service';
+import { Router, ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-edit-room',
   templateUrl: './edit-room.component.html',
   styleUrls: ['./edit-room.component.css']
 })
 export class EditRoomComponent implements OnInit {
+  private myId;
+  private MyDataSource = {};
 
-  constructor() { }
+  constructor(private roomManagement: RoomManagementService, private _router: Router,private activatedRoute: ActivatedRoute) { 
+    this.myId = this.activatedRoute.snapshot.params.id;
+    const preParam = {
+      id: this.myId
+    };
+    this.roomManagement.getExamRoom(preParam).then(response => {
+      this.MyDataSource = response;
+    });
+  }
   buildings = [
     { value: 'อาคาร 1', viewValue: 'อาคาร 1' },
     { value: 'อาคาร 2', viewValue: 'อาคาร 2' },
