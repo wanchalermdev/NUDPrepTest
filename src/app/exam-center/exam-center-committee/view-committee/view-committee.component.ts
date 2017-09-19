f6c053e7566c1e66f7efb1124c656062f710551f
wanchalermdev <wanchalerm.dev@gmail.com>
2017-09-19 10:50:02 +0700

    ปรับปรุงการจัดการบัญชีผู้ใช้

--- src/app/exam-center/exam-center-committee/view-committee/view-committee.component.ts
+++ src/app/exam-center/exam-center-committee/view-committee/view-committee.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { CommitteeManagementService } from '../../../service/committee-management.service';
+import { BuildingManagementService } from '../../../service/building-management.service';
+import { RoomManagementService } from '../../../service/room-management.service';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-view-committee',
@@ -7,7 +11,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViewCommitteeComponent implements OnInit {
 
-  constructor() { }
+  private name;
+  private room;
+  private building;
+  private myId;
+  private _school_id;
+
+  private responseBuilding;
+  private responseCommittee;
+  private responseExamRoom;
+  constructor(
+    private committeeManagement: CommitteeManagementService,
+    private buildingManagement: BuildingManagementService,
+    private _router: Router,
+    private activatedRoute: ActivatedRoute,
+    private roomManagementService: RoomManagementService
+  ) {
+    this.myId = activatedRoute.snapshot.params.id;
+    this._school_id = window.sessionStorage.getItem('PSN_SCHOOL_ID');
+    const preParamCommittee = {
+      id: this.myId,
+      school_id: this._school_id
+    };
+    this.committeeManagement.getCommittee(preParamCommittee).then(response => {
+      console.log(response);
+      this.responseCommittee = response;
+    });
+    // const _building_id = this.responseBuilding['building_id'];
+    // const preParamBuilding = {
+    //   id: _building_id,
+    //   school_id: this._school_id
+    // };
+    // this.buildingManagement.getBuilding(preParamBuilding).then(response => {
+    //   this.responseBuilding = response;
+    // });
+
+    //this.name = this.responseCommittee['committee_prename'] + this.responseCommittee['committee_firstname'];
+  }
 
   ngOnInit() {
   }
