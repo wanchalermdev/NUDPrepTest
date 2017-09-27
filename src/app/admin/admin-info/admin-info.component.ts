d1cff7a207b37c634eefadca34531faa78ef269a
wanchalermdev <wanchalerm.dev@gmail.com>
2017-09-27 13:26:42 +0700

    ใส่ cahrt ใน componenet ได้แล้ววววววว

--- src/app/admin/admin-info/admin-info.component.ts
+++ src/app/admin/admin-info/admin-info.component.ts
@@ -8,6 +8,7 @@ import { ViewChild } from '@angular/core';
 import 'rxjs/add/operator/startWith';
 import 'rxjs/add/observable/merge';
 import 'rxjs/add/operator/map';
+import { Chart } from 'chart.js/dist/Chart.js';
 
 @Component({
   selector: 'app-admin-info',
@@ -18,7 +19,7 @@ export class AdminInfoComponent implements OnInit {
 
 
 
-  constructor(private login:LoginService) { }
+  constructor(private login: LoginService) { }
 
   displayedColumns = ['userId', 'userName', 'progress', 'color'];
   exampleDatabase = new ExampleDatabase();
@@ -28,53 +29,92 @@ export class AdminInfoComponent implements OnInit {
 
   ngOnInit() {
     this.dataSource = new ExampleDataSource(this.exampleDatabase, this.paginator);
+
+    var canvas : any = document.getElementById("myChart");
+    var ctx = canvas.getContext("2d");
+    var myChart = new Chart(ctx, {
+      type: 'bar',
+      data: {
+        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        datasets: [{
+          label: '# of Votes',
+          data: [12, 19, 3, 5, 2, 3],
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)'
+          ],
+          borderColor: [
+            'rgba(255,99,132,1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)'
+          ],
+          borderWidth: 1
+        }]
+      },
+      options: {
+        scales: {
+          yAxes: [{
+            ticks: {
+              beginAtZero: true
+            }
+          }]
+        }
+      }
+    });
   }
 }
 /** Constants used to fill up our data base. */
 const COLORS = ['maroon', 'red', 'orange', 'yellow', 'olive', 'green', 'purple',
-'fuchsia', 'lime', 'teal', 'aqua', 'blue', 'navy', 'black', 'gray'];
+  'fuchsia', 'lime', 'teal', 'aqua', 'blue', 'navy', 'black', 'gray'];
 const NAMES = ['Maia', 'Asher', 'Olivia', 'Atticus', 'Amelia', 'Jack',
-'Charlotte', 'Theodore', 'Isla', 'Oliver', 'Isabella', 'Jasper',
-'Cora', 'Levi', 'Violet', 'Arthur', 'Mia', 'Thomas', 'Elizabeth'];
+  'Charlotte', 'Theodore', 'Isla', 'Oliver', 'Isabella', 'Jasper',
+  'Cora', 'Levi', 'Violet', 'Arthur', 'Mia', 'Thomas', 'Elizabeth'];
 
 export interface UserData {
-id: string;
-name: string;
-progress: string;
-color: string;
+  id: string;
+  name: string;
+  progress: string;
+  color: string;
 }
 
 /** An example database that the data source uses to retrieve data for the table. */
 export class ExampleDatabase {
-/** Stream that emits whenever the data has been modified. */
-dataChange: BehaviorSubject<UserData[]> = new BehaviorSubject<UserData[]>([]);
-get data(): UserData[] { return this.dataChange.value; }
+  /** Stream that emits whenever the data has been modified. */
+  dataChange: BehaviorSubject<UserData[]> = new BehaviorSubject<UserData[]>([]);
+  get data(): UserData[] { return this.dataChange.value; }
 
-constructor() {
-  // Fill up the database with 100 users.
-  for (let i = 0; i < 100; i++) { this.addUser(); }
-}
+  constructor() {
+    // Fill up the database with 100 users.
+    for (let i = 0; i < 100; i++) { this.addUser(); }
+  }
 
-/** Adds a new user to the database. */
-addUser() {
-  const copiedData = this.data.slice();
-  copiedData.push(this.createNewUser());
-  this.dataChange.next(copiedData);
-}
+  /** Adds a new user to the database. */
+  addUser() {
+    const copiedData = this.data.slice();
+    copiedData.push(this.createNewUser());
+    this.dataChange.next(copiedData);
+  }
 
-/** Builds and returns a new User. */
-private createNewUser() {
-  const name =
-    NAMES[Math.round(Math.random() * (NAMES.length - 1))] + ' ' +
-    NAMES[Math.round(Math.random() * (NAMES.length - 1))].charAt(0) + '.';
+  /** Builds and returns a new User. */
+  private createNewUser() {
+    const name =
+      NAMES[Math.round(Math.random() * (NAMES.length - 1))] + ' ' +
+      NAMES[Math.round(Math.random() * (NAMES.length - 1))].charAt(0) + '.';
 
-  return {
-    id: (this.data.length + 1).toString(),
-    name: name,
-    progress: Math.round(Math.random() * 100).toString(),
-    color: COLORS[Math.round(Math.random() * (COLORS.length - 1))]
-  };
-}
+    return {
+      id: (this.data.length + 1).toString(),
+      name: name,
+      progress: Math.round(Math.random() * 100).toString(),
+      color: COLORS[Math.round(Math.random() * (COLORS.length - 1))]
+    };
+  }
 }
 
 /**
@@ -85,25 +125,25 @@ private createNewUser() {
 * should be rendered.
 */
 export class ExampleDataSource extends DataSource<any> {
-constructor(private _exampleDatabase: ExampleDatabase, private _paginator: MdPaginator) {
-  super();
-}
+  constructor(private _exampleDatabase: ExampleDatabase, private _paginator: MdPaginator) {
+    super();
+  }
 
-/** Connect function called by the table to retrieve one stream containing the data to render. */
-connect(): Observable<UserData[]> {
-  const displayDataChanges = [
-    this._exampleDatabase.dataChange,
-    this._paginator.page,
-  ];
+  /** Connect function called by the table to retrieve one stream containing the data to render. */
+  connect(): Observable<UserData[]> {
+    const displayDataChanges = [
+      this._exampleDatabase.dataChange,
+      this._paginator.page,
+    ];
 
-  return Observable.merge(...displayDataChanges).map(() => {
-    const data = this._exampleDatabase.data.slice();
+    return Observable.merge(...displayDataChanges).map(() => {
+      const data = this._exampleDatabase.data.slice();
 
-    // Grab the page's slice of data.
-    const startIndex = this._paginator.pageIndex * this._paginator.pageSize;
-    return data.splice(startIndex, this._paginator.pageSize);
-  });
-}
+      // Grab the page's slice of data.
+      const startIndex = this._paginator.pageIndex * this._paginator.pageSize;
+      return data.splice(startIndex, this._paginator.pageSize);
+    });
+  }
 
-disconnect() { }
+  disconnect() { }
 }
