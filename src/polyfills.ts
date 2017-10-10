9482a638eef357abbb5b97dc159a291c07769173
wanchalermdev <wanchalerm.dev@gmail.com>
2017-10-10 13:33:28 +0700

    Revert "ใส่ cahrt ใน componenet ได้แล้ววววววว"
    This reverts commit d1cff7a207b37c634eefadca34531faa78ef269a.

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -36,7 +36,6 @@
 
 /** IE10 and IE11 requires the following for NgClass support on SVG elements */
 // import 'classlist.js';  // Run `npm install --save classlist.js`.
-import 'assets/chart/chart.js';
 
 /** Evergreen browsers require these. **/
 import 'core-js/es6/reflect';
